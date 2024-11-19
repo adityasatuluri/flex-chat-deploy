9a3544efbcc4ff59b69bdcce199f8232fd436b94
Aditya Satuluri <s.aditya.in@gmail.com>
2024-11-19 20:05:24 +0530

    next() in middleware

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -1,18 +1,19 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.jwt;
-    console.log('Token:', token);
-  
-    if (!token) {
-      return res.status(401).send('You are not authenticated!');
+  const token = req.cookies.jwt;
+  console.log("Token:", token);
+  next();
+
+  if (!token) {
+    return res.status(401).send("You are not authenticated!");
+  }
+
+  jwt.verify(token, process.env.JWT_KEY, async (err, decoded) => {
+    if (err) {
+      return res.status(403).send("Token is not valid!");
     }
-  
-    jwt.verify(token, process.env.JWT_KEY, async(err, decoded) => {
-      if (err) {
-        return res.status(403).send('Token is not valid!');
-      }
-      req.userId = decoded.userId;
-      next();
-    });
-  };
+    req.userId = decoded.userId;
+    next();
+  });
+};
